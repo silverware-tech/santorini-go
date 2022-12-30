63d8ba3d3ab1cc312e00b0db61786819ae52026e
Daniele Zulberti <danielezulberti@gmail.com>
2022-12-30 18:52:56 +0100

    Struct and matrix example
    Co-authored-by: Lorenzo Ganni <c2r0b@users.noreply.github.com>
    Co-authored-by: Luca Argentieri <SilverLuke@users.noreply.github.com>

--- hello.go
+++ hello.go
@@ -2,13 +2,50 @@ package main
 
 import "fmt"
 
-func test(test string) {
-	fmt.Println(test)
+const SIZE = 5
+
+type Player struct {
+	x int
+	y int
+	group int
+	name string
 }
 
-func main() {
-	var testvar = "test"
 
-	fmt.Println("Hello, World!")
-	test(testvar)
+
+func main() {
+	var field[SIZE][SIZE]int
+	player1 := Player{
+		x:0,
+		y:0,
+		group: 0,
+        name: "Player1",
+    }
+    player2 := Player{
+        x:1,
+        y:0,
+        group: 1,
+        name: "Player2",
+    }
+    player3 := Player{
+        x:2,
+        y:0,
+        group: 2,
+        name: "Player3",
+    }
+
+    for i := 0; i < SIZE; i++ {
+        for j := 0; j < SIZE; j++ {
+            field[i][j] = 0
+        }
+    }
+
+	fmt.Println("Hello, Santorini!", player1, player2, player3)
+
+    for i := 0; i < SIZE; i++ {
+        for j := 0; j < SIZE; j++ {
+            fmt.Print(field[i][j])
+        }
+        fmt.Println()
+    }
 }
